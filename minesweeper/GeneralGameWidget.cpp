7863c8e480bb1267455d007bf1d3f2c076e8725c
jeang-bo-yuan <jeangbyj@gmail.com>
2023-04-15 21:47:55 +0800

    避免重覆設置layout

--- minesweeper/GeneralGameWidget.cpp
+++ minesweeper/GeneralGameWidget.cpp
@@ -8,26 +8,26 @@
 
 
 GeneralGameWidget::GeneralGameWidget(GameBoard* p, const std::string s,QWidget* parent)
-    : QWidget(parent), board_p(p), state(s), vLayout(new QVBoxLayout(this)), infoBox(new QGridLayout(this))
+    : QWidget(parent), board_p(p), state(s), vLayout(new QVBoxLayout(this)), infoBox(new QGridLayout)
 {
 // information
     // game state
-    infoBox->addWidget(new QLabel("Game State : ", this), 0, 0);
-    infoBox->addWidget(new QLabel(state.c_str(), this), 0, 1);
+    infoBox->addWidget(new QLabel("Game State : "), 0, 0);
+    infoBox->addWidget(new QLabel(state.c_str()), 0, 1);
     vLayout->addLayout(infoBox);
 
 // for print
-    QHBoxLayout* printBox = new QHBoxLayout(this);
+    QHBoxLayout* printBox = new QHBoxLayout;
     // print gameboard
-    QPushButton* forBoard = new QPushButton("Print GameBoard", this);
+    QPushButton* forBoard = new QPushButton("Print GameBoard");
     forBoard->setMinimumSize(150, 30);
     QObject::connect(forBoard, &QPushButton::clicked, this, [this]() {printCommand("GameBoard", *(this->board_p), this->state.c_str());});
     // print gameanswer
-    QPushButton* forAnswer = new QPushButton("Print GameAnswer", this);
+    QPushButton* forAnswer = new QPushButton("Print GameAnswer");
     forAnswer->setMinimumSize(150, 30);
     QObject::connect(forAnswer, &QPushButton::clicked, this, [this]() {printCommand("GameAnswer", *(this->board_p), this->state.c_str());});
     // print gamestate
-    QPushButton* forState = new QPushButton("Print GameState", this);
+    QPushButton* forState = new QPushButton("Print GameState");
     forState->setMinimumSize(150, 30);
     QObject::connect(forState, &QPushButton::clicked, this, [this]() {printCommand("GameState", *(this->board_p), this->state.c_str());});
     //
@@ -35,7 +35,5 @@ GeneralGameWidget::GeneralGameWidget(GameBoard* p, const std::string s,QWidget* 
     printBox->addWidget(forAnswer);
     printBox->addWidget(forState);
     vLayout->addLayout(printBox);
-
-    this->setLayout(vLayout);
 }
 
