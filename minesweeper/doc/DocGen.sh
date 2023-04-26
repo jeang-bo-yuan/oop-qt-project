76c6559f86a9fa122b2efef17f96fb73a791d024
jeang-bo-yuan <jeangbyj@gmail.com>
2023-04-26 14:03:34 +0800

    改進DocGen.sh

--- minesweeper/doc/DocGen.sh
+++ minesweeper/doc/DocGen.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/sh
 
+case $1 in
+	"--dot")
+		# for Include_Path.svg
+		cinclude2dot --src=.. --quotetypes=quote > Include_Path.dot
+		dot Include_Path.dot -Tsvg -o Include_Path.svg  # dot is from graphviz
+		;;
+	"-h"|"--help")
+		echo "Usage:"
+		echo "	./DocGen.sh [--dot]"
+		exit 0
+		;;
+esac
+
 # for .chm
 [ -d html ] && rm -r html
 doxygen
 hhc html/index.hhp # hhc is from HTML Help Workshop
 mv html/index.chm Document.chm
-
-# for Include_Path.svg
-cinclude2dot --src=.. --quotetypes=quote > Include_Path.dot
-dot Include_Path.dot -Tsvg -o Include_Path.svg  # dot is from graphviz
